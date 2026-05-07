86778065d45e8db51f8234fca9aa3d79ee9a5bde
Ji, jeonguk <dkdpawjddnr@naver.com>
2026-05-07 23:36:53 +0900

    feat: 2주차 LinkedList 데이터 추가(append) 및 출력(printAll) 기능 구현

--- src/second_week/LinkedListCreate.java
+++ src/second_week/LinkedListCreate.java
@@ -17,7 +17,22 @@ class LinkedListCr {
         head = new Node(value);
     }
 
-}
+    public void append(int value){
+        Node cur = head;
+        while(cur.next != null){
+            cur = cur.next;
+        }
+        cur.next = new Node(value);
+    }
+
+    public void printAll(){
+        Node cur = head;
+        while(cur != null){
+            System.out.println(cur.data);
+            cur = cur.next;
+        }
+    }
+
 
 public class LinkedListCreate{
     public static void main(String[] args) {
