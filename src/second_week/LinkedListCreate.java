ab0f85f33de917366e3683b664114f20ebef164d
Ji, jeonguk <dkdpawjddnr@naver.com>
2026-05-09 01:09:34 +0900

    feat: 2주차 LinkedList 특정 인덱스 노드 조회(getNode) 기능 추가

--- src/second_week/LinkedListCreate.java
+++ src/second_week/LinkedListCreate.java
@@ -33,10 +33,25 @@ class LinkedListCr {
         }
     }
 
+    public Node getNode(int index){
+        int curIndex = 0;
+        Node curNode = head;
+
+        while(curIndex != index){
+            curNode = curNode.next;
+            curIndex++;
+        }
+        return curNode;
+    }
+}
 
 public class LinkedListCreate{
     public static void main(String[] args) {
         LinkedListCr linkedList = new LinkedListCr(5);
-
+        linkedList.append(12);
+        linkedList.append(8);
+        linkedList.printAll();
+        LinkedListCr.Node getNode = linkedList.getNode(1);
+        System.out.println("getNode : " + getNode.data);
     }
 }
